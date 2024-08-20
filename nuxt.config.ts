5e51fe8881fc4d34307980c39acf657298937401
Soe Thura <thixpin@gmail.com>
2024-08-20 10:48:21 +0630

    disable: devtools

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true },
+  devtools: { enabled: process.env.NODE_ENV !== 'production' },
   extends: [
     '@nuxt/examples-ui'
   ],
